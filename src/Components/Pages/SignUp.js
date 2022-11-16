ebf8bc93025c92c9047897c5cc8f192b2ab07f12
Apoorva Raj <92389929+Apoorva2520@users.noreply.github.com>
2022-11-16 22:19:27 +0530

    Update SignUp.js

--- src/Components/Pages/SignUp.js
+++ src/Components/Pages/SignUp.js
@@ -1,19 +1,18 @@
 import React from 'react';
 import '../../App.css';
-// import { Button } from './Button';
+import Cards from '../Cards';
+import HeroSection from '../HeroSection';
+import Footer from '../Footer';
 
-export default function SignUp() {
+function SignUp() {
   return (
     <>
-  <h1 className='sign-up'>LIKE & SUBSCRIBE</h1>
-  {/* // <div className='-btns'>
-  //       <Button
-  //         className='btns'
-  //         buttonStyle='btn--outline'
-  //         buttonSize='btn--large'>
-  //         GET STARTED
-  //       </Button>
-  //     </div> */}
-  </>
+      <HeroSection/>
+      <Cards />
+      <Footer />
+      
+    </>
   );
 }
+
+export default SignUp;
