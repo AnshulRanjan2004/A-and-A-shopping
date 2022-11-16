bfc8167f57dbc5af8289f84659200c414d0ae0e4
Apoorva Raj <92389929+Apoorva2520@users.noreply.github.com>
2022-11-16 22:17:11 +0530

    Update Bottoms.js

--- src/Components/Pages/Bottoms.js
+++ src/Components/Pages/Bottoms.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import '../../App.css';
 import Cards from '../Cards';
+import HeroSection from '../HeroSection';
 import Footer from '../Footer';
 
 function Bottoms() {
   return (
     <>
+      <HeroSection />
       <Cards />
       <Footer />
       
