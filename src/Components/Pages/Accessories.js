5dead71ea6a79fd70de9c93746b59d11d6a5d751
Apoorva Raj <92389929+Apoorva2520@users.noreply.github.com>
2022-11-16 22:16:15 +0530

    Update Accessories.js

--- src/Components/Pages/Accessories.js
+++ src/Components/Pages/Accessories.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import '../../App.css';
 import Cards from '../Cards';
-
+import HeroSection from '../HeroSection';
 import Footer from '../Footer';
 
 function Accessories() {
   return (
     <>
+      <HeroSection />
       <Cards />
       <Footer />
       
