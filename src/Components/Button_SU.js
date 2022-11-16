b589ffa39b5ba4189f6a72839194d815be50f4fd
Apoorva Raj <92389929+Apoorva2520@users.noreply.github.com>
2022-11-16 22:12:41 +0530

    Update Button_SU.js

--- src/Components/Button_SU.js
+++ src/Components/Button_SU.js
@@ -1,48 +1,13 @@
-import React from 'react';
-import './Button.css';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Button.css";
 
-const STYLES = ['btn--primary', 'btn--outline', 'btn--test'];
-
-const SIZES = ['btn--medium', 'btn--large'];
-
-export const Button = ({
-  children,
-  type,
-  onClick,
-  buttonStyle,
-  buttonSize
-}) => {
-  const checkButtonStyle = STYLES.includes(buttonStyle)
-    ? buttonStyle
-    : STYLES[0];
-
-  const checkButtonSize = SIZES.includes(buttonSize) ? buttonSize : SIZES[0];
-
+function Button_SU() {
   return (
-    <Link to='/products' className='btn-mobile'>
-      <button
-        className={`btn ${checkButtonStyle} ${checkButtonSize}`}
-        onClick={onClick}
-        type={type}
-      >
-        {children}
-      </button>
+    <Link to="signup">
+      <button className="btn-cart">Sign Up</button>
     </Link>
   );
-};
+}
 
-
-// import React from "react";
-// import { Link } from "react-router-dom";
-// import "./Button.css";
-
-// function Button() {
-//   return (
-//     <Link to="signup">
-//       <button className="btn">Sign Up</button>
-//     </Link>
-//   );
-// }
-
-// export default Button;
+export default Button_SU;
