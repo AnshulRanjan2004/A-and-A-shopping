60ddcb75e850de9294f9a422b83d1e31a86514ed
Anvesha Nayak <anveshalnayak@gmail.com>
2022-11-28 11:17:28 +0530

    anshuliii

--- src/Components/Pages/SignUp.js
+++ src/Components/Pages/SignUp.js
@@ -1,18 +1,36 @@
 import React from 'react';
 import '../../App.css';
-import Cards from '../Cards';
-import HeroSection from '../HeroSection';
-import Footer from '../Footer';
+import './style.css';
 
-function SignUp() {
-  return (
-    <>
-      <HeroSection/>
-      <Cards />
-      <Footer />
-      
-    </>
-  );
-}
+export const SignUp = () => {
+  return(
+  <center>
+  <div>
+      <h1><b>Create an Account</b></h1>
+      <div class="signup-box">
+          <div class="left-box">
+              <form>
+                  <input type="text" placeholder="Your Name" required class="input-box"/>
+                  <input type="text" placeholder="Username" required class="input-box"/>
+                  <input type="email" placeholder="Your email id" required class="input-box"/>
+                  <input type="password" placeholder="Create Password" required class="input-box"/>
+                  <input type="checkbox" id="terms"/>
+                  <label for="terms">I accept the terms & conditions.</label>
+                  <button type="submit">SIGN UP <span>&#x27f6;</span></button>
+              </form>
+          </div>
+          <div class="right-box">
+              <a href="" class="fb-link"><i class="fab fa-facebook-f"></i> Connect with Facebook</a>
+              <a href="" class="tw-link"><i class="fab fa-twitter"></i> Connect with Twitter</a>
+              <a href="" class="g-link"><i class="fab fa-google"></i> Connect with Google</a>
+          </div>
+      </div>
+      <p class="login-msg">I already have an account? <a href="#">Login Now</a></p>
 
+  </div>
+  </center>
+);
+};
 export default SignUp;
+
+
